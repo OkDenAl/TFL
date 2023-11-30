7e8b1339e027eb710e1fdc3a0735a6a191677e72
Denis <denok629@gmail.com>
2023-11-30 18:48:59 +0300

    add new errors

--- dop-task/main.cpp
+++ dop-task/main.cpp
@@ -2,8 +2,11 @@
 #include <vector>
 #include <iostream>
 #include <memory>
+#include <algorithm>
+#include <unordered_set>
 
-class string // ошибка на строке 28 фиксится тем, что сюда нужно добавить;
+
+class string // ошибка на строке 31 фиксится тем, что сюда нужно добавить;
 
 class RegexGenerator {
     friend class StringGenerator;
@@ -27,7 +30,7 @@ public:
     RegexGenerator(int lettersNum, int starNesting,int lookaheadNum, int lookbehindNum,int alphabetSize, bool needRestrictionsForLookbehind=true);
 };
 
-RegexGenerator::RegexGenerator(): // ошибки на строках 31,32,33 фиксится тем, что нужно убрать вот эту строку
+RegexGenerator::RegexGenerator(): // ошибки на строках 34,35,36 фиксится тем, что нужно убрать вот эту строку
 RegexGenerator::RegexGenerator(int lettersNum, int starNesting,int lookaheadNum,
                                int lookbehindNum,int alphabetSize,bool needRestrictionsForLookbehind)
         :lettersNum(lettersNum),starNesting(starNesting),lookaheadNum(lookaheadNum),
@@ -61,7 +64,7 @@ class IClass{
     virtual void Test()=0;
 };
 
-class Class1:IClass { // ошибка на строке 76 фиксится тем, что сюда нужно написать public
+class Class1:IClass { // ошибка на строке 106 фиксится тем, что сюда нужно написать public
     int p;
     void Test() override{
         std::cout<<"From test\n";
@@ -74,7 +77,62 @@ std::string GenerateRegex( std::unique_ptr<IClass> b) {
     return "";
 }
 
+template<class It>
+void sort2(It first, It last) {
+    std::sort(first, last);
+}
+
+struct B{
+    std::string test;
+    std::string test1;
+    std::string test2;
+};
+
+class Cl{
+public:
+    std::unordered_set<B> k; // тут надо хеш функцию для B опреедлять и оператор ==
+    Cl();
+};
+
+
 int  main(){
     Testing w;
+    std::vector<int> a(10);
+
+    sort2(1,2);
+//    sort2(a.begin(),a.end()); // исправление ошибки на строке 101
+    Cl b;
     GenerateRegex(std::make_unique<Class1>(Class1 (1)));
 }
+
+
+// чтобы не сбивать main, вынес сюда
+class StateMachine{
+private:
+    int stateCount;
+    std::vector<std::vector<char>> transitions;
+    std::unordered_set<int> finalStates;
+
+public:
+    void AddTransition(int curState, char curSignal, int nextState // тут типа забыл скобку круглую и ;
+    void SetFinalStates(std::unordered_set<int> final);
+
+    ~StateMachine()=default;
+
+    int GetStateNum() const;
+};
+
+
+int StateMachine::GetStateNum() const{
+    return stateCount;
+// типа забыл скобку
+
+void StateMachine::AddTransition(int curState, char curSignal, int nextState){
+    transitions[curState][nextState]=curSignal;
+}
+
+void StateMachine::SetFinalStates(std::unordered_set<int> final) {
+    finalStates=std::move(final);
+}
+
+
